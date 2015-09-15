15fb10956e7484e8d5e9b6551ea5a02fb864afcc
amao12580 <chengliangchengliang888@gmail.com>
2015-09-15 16:34:32 +0800

    工程初始化

---
+++ core.base/src/main/java/per/rss/core/base/App.java
@@ -0,0 +1,13 @@
+package per.rss.core.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
